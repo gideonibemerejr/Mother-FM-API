ed9e51aee8403cf390798154b20f7c409ecc2f64
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-10 17:05:02 -0500

    Initialize with Controllers, Models, and Routes. Adjust server.js to port 8000

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mixesRouter = require('./routes/api/mixes')
+const usersRouter = require('./routes/users')
 
-var app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+require('dotenv').config()
+require('./config/database')
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-module.exports = app;
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/mixes', mixesRouter)
+app.use('/users', usersRouter)
+
+module.exports = app
