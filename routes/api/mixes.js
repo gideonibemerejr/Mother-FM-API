d6cbe7978097dd831de2ce144040ec99c5a4f33f
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-07-11 01:53:20 -0500

    Add final changes to mixes route and auth.js

--- routes/api/mixes.js
+++ routes/api/mixes.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const mixesCtrl = require('../../controllers/api/mixes')
 
 // * Public Routes
-router.get('/')
+router.get('/', mixesCtrl.getMixes)
+
+// ! Protected Routes
+// Process the token for only the protected routs
+
+router.use(require('../../config/auth'))
+router.get('/:id', mixesCtrl.getOne)
+router.get('/archive', mixesCtrl.getAllMixes)
+router.post('/', mixesCtrl.create)
+router.delete('/', mixesCtrl.deleteMix)
+router.put('/', mixesCtrl.update)
 
 module.exports = router
