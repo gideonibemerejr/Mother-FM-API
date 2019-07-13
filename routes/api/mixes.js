fddb78543f17ef271c4f2cbc4bfbea349e9afd73
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-07-13 03:00:48 -0500

    Update

--- routes/api/mixes.js
+++ routes/api/mixes.js
@@ -4,12 +4,12 @@ const mixesCtrl = require('../../controllers/api/mixes')
 
 // * Public Routes
 router.get('/', mixesCtrl.getMixes)
-router.get('/:id', mixesCtrl.getOne)
+router.get('/all', mixesCtrl.getAllMixes)
+// router.get('/:id', mixesCtrl.getOne)
 
 // ! Protected Routes
 // Process the token for only the protected routes
 router.use(require('../../config/auth'))
-router.get('/archive', mixesCtrl.getAllMixes)
 router.post('/', checkAuth, mixesCtrl.create)
 //router.delete('/', checkAuth, mixesCtrl.deleteMix)
 //router.put('/', checkAuth, mixesCtrl.update)
